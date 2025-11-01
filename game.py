5be28cac47f85904c2e4f22745edb7e80f20daaa
Jonathan Lyria <jon.lyria@gmail.com>
2025-11-01 22:46:24 +0000

    unfinished agent.py

--- game.py
+++ game.py
@@ -8,26 +8,23 @@ class Game:
         self.gameState = {} #key: id, value: list of key information in the order: name, is still in game
 
     def initialise_game_state(self, list_of_agents : List[a], player: p):
-        self.gameState["0"] = [player.figure, True]
         for agent in list_of_agents:
-            self.gameState[agent.id] = [agent.currentFigure, False]
+            self.gameState[agent] = False
 
     def update_voted_out(self, votes: Dict[str, int]):
         player_voted_out = max(votes, key=votes.get)
         self.gameState[player_voted_out][1] = False
 
+    def update_suspicion_levels(self, response: msg):
+        for agent in self.gameState:
+            if agent.id != response.sender:
+                agent.check_suspicion(response.sender, response.content)
 
-    def gamePlay(self):
-        agent1 = a("", "", )
+    def answer_question(self, question: msg, senderID, recipientID):
+        recipientAgent = gameState[]
 
-    def play_round(self, response: msg):
-        responseMessage = response.content
-        senderId = response.id
-        responseContent = response.content
-        responseType = response.type
-        responseSender = response.sender
+    def
 
-        for
 
 
 
