031458cc9efd3c468bdf52003953cf3f51b99dac
Jonathan Lyria <jon.lyria@gmail.com>
2025-11-01 21:49:58 +0000

    unfinished game.py

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
-from agent import Agent
-import message as msg
-from player import Player
+from agent import Agent as a
+from message import Message as msg
+from player import Player as p
 from typing import List, Dict
 import json
 class Game:
     def __init__(self, ):
         self.gameState = {} #key: id, value: list of key information in the order: name, is still in game
 
-    def initialise_game_state(self, list_of_agents : List[Agent], player: Player):
+    def initialise_game_state(self, list_of_agents : List[a], player: p):
         self.gameState["0"] = [player.figure, True]
         for agent in list_of_agents:
             self.gameState[agent.id] = [agent.currentFigure, False]
@@ -15,3 +15,10 @@ class Game:
     def update_voted_out(self, votes: Dict[str, int]):
         player_voted_out = max(votes, key=votes.get)
         self.gameState[player_voted_out][1] = False
+
+    def play_round(self, response: msg):
+        message = response.
+
+
+
+
